ff4f47ffdbe8179da4855865fa4b100a002b2c20
Evaristus Maduhu <evaristus.maduhu@ega.go.tz>
2026-05-28 16:39:58 +0300

    Force app updates from store

--- flutter_app/lib/services/update_check_service.dart
+++ flutter_app/lib/services/update_check_service.dart
@@ -3,6 +3,7 @@ import 'package:upgrader/upgrader.dart';
 
 class UpdateCheckService {
   static final Map<String, Upgrader> _cache = {};
+  static const String minimumSupportedVersion = '1.0.1';
 
   static Upgrader upgraderFor(Locale locale) {
     final languageCode = locale.languageCode;
@@ -14,11 +15,37 @@ class UpdateCheckService {
               debugDisplayOnce: false,
               debugLogging: false,
               languageCode: languageCode,
+              minAppVersion: minimumSupportedVersion,
+              showOnlyMandatoryUpdates: false,
               messages: languageCode == 'sw'
                   ? SwahiliUpgraderMessages()
-                  : UpgraderMessages(code: 'en'),
+                  : ForcedEnglishUpgraderMessages(),
               durationUntilAlertAgain: Duration.zero,
             ));
+  }
+}
+
+class ForcedEnglishUpgraderMessages extends UpgraderMessages {
+  ForcedEnglishUpgraderMessages() : super(code: 'en');
+
+  @override
+  String? message(UpgraderMessage messageKey) {
+    switch (messageKey) {
+      case UpgraderMessage.body:
+        return 'A new version of {{appName}} is available. Version {{currentAppStoreVersion}} is ready; you are using {{currentInstalledVersion}}.';
+      case UpgraderMessage.buttonTitleIgnore:
+        return 'Ignore';
+      case UpgraderMessage.buttonTitleLater:
+        return 'Later';
+      case UpgraderMessage.buttonTitleUpdate:
+        return 'Update now';
+      case UpgraderMessage.prompt:
+        return 'You must update the app to continue using the service.';
+      case UpgraderMessage.releaseNotes:
+        return 'Release notes';
+      case UpgraderMessage.title:
+        return 'Update required';
+    }
   }
 }
 
